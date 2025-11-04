97fea2267388a6cb9c61a606c4382ef0da9e368f
MaxZigman <puckers.formal_0u@icloud.com>
2025-11-04 11:26:15 +0100

    new: Traingsset_prep

--- data/Trainingsset_prep.py
+++ data/Trainingsset_prep.py
@@ -2,4 +2,7 @@ import numpy as np
 
 dataset = np.loadtxt("data/Trainingsset.csv", delimiter=",")
 selected_column = dataset[:, :2]
-np.savetxt("Trainingsset_prep.csv", selected_column, delimiter=",")
+dataset[:, [0, 1]] = dataset[:, [1, 0]]
+
+np.savetxt("data/Trainingsset_prep.csv", selected_column, delimiter=",")
+
